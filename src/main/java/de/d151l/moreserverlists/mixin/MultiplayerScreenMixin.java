19d5588de16a051516beb06aeaf9377b4a40a9e2
Dominic <d151l@d151l.de>
2023-09-24 21:09:26 +0200

    idk

--- src/main/java/de/d151l/moreserverlists/mixin/MultiplayerScreenMixin.java
+++ src/main/java/de/d151l/moreserverlists/mixin/MultiplayerScreenMixin.java
@@ -8,7 +8,6 @@ import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -17,8 +16,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MultiplayerScreenMixin extends Screen {
 
     private Screen parent;
-
-    private Text title = Text.of("");
 
     private final int arrowWidth = 20;
     private final int listNameWidth = 130;
